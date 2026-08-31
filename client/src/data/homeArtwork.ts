ea8fbcd5eb6c2fa56868068778c369983c40d468
Himian109 <khun77109@gmail.com>
2026-08-31 22:34:27 +0800

    feat: add official homepage artwork map

---
+++ client/src/data/homeArtwork.ts
@@ -0,0 +1,12 @@
+export const homeArtwork: Record<string, string> = {
+  Babymarutchi: "https://tamagotchi-official.com/tamagotchi/jp/character/2025/05/07/O3Q1qX7zMPgtYExb/01_%E3%81%B9%E3%81%B3%E3%81%BE%E3%82%8B%E3%81%A1%E3%81%A3%E3%81%A1.png",
+  Meowtchi: "https://tamagotchi-official.com/tamagotchi/jp/character/2025/05/07/2lNIYfe6Fr3pKTcC/18_%E3%81%BF%E3%82%83%E3%81%8A%E3%81%A3%E3%81%A1_LAND.png",
+  Irukatchi: "https://tamagotchi-official.com/tamagotchi/jp/character/2025/05/07/3lgIUTgfN1oa1JPE/34_%E3%81%84%E3%82%8B%E3%81%8B%E3%81%A3%E3%81%A1_WATER.png",
+  Horhotchi: "https://tamagotchi-official.com/tamagotchi/jp/character/2025/05/07/we5Yc497XcMID13K/50_%E3%81%BB%E3%83%BC%E3%81%BB%E3%81%A3%E3%81%A1_SKY.png",
+  "Forest Horhotchi": "https://tamagotchi-official.com/tamagotchi/jp/character/2025/10/09/MfNvzCMYBRckJPdi/82_%E3%82%82%E3%82%8A%E3%81%AE%E3%81%BB%E3%83%BC%E3%81%BB%E3%81%A3%E3%81%A1_JADE.png",
+  Ananatchi: "https://tamagotchi-official.com/tamagotchi/jp/character/2026/05/11/oswGLZFTAk7V2xXS/%E3%81%82%E3%81%AA%E3%81%AA%E3%81%A3%E3%81%A1_TROPICS.png",
+  "Icy Irukatchi": "https://tamagotchi-official.com/tamagotchi/jp/character/2026/05/13/PZTB4gCiFuaWVnLH/%E3%82%B7%E3%83%AD%E3%81%84%E3%82%8B%E3%81%8B%E3%81%A3%E3%81%A1_GLACIER.png",
+  Mimitchi: "https://tamagotchi-official.com/tamagotchi/jp/character/2025/05/07/jOxYtX6kq89MZuKz/23_%E3%81%BF%E3%81%BF%E3%81%A3%E3%81%A1_LAND.png",
+  Mametchi: "https://tamagotchi-official.com/tamagotchi/jp/character/2025/05/07/x5L4gBAu0ulur7Lp/22_%E3%81%BE%E3%82%81%E3%81%A3%E3%81%A1_LAND.png",
+  Kuchipatchi: "https://tamagotchi-official.com/tamagotchi/jp/character/2025/05/07/7pLcB0f1vhrrGZcN/58_%E3%81%8F%E3%81%A1%E3%81%B1%E3%81%A3%E3%81%A1_SKY.png",
+};
